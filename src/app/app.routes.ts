d1fb0384cb6402c159fc46df9e5c9d9c7e22dad8
RichardQuing <richardquinaucho65@gmail.com>
2024-08-20 10:27:02 -0500

    se arreglo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { PokemonComponent } from './pages/pokemon/pokemon.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'pokemons', component: PokemonComponent},
     {path: 'acercade', component: AcercadeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
