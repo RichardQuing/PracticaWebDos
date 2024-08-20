f10ffd41d8cc9abccb78c3ebb00570d35fcb3b45
RichardQuing <richardquinaucho65@gmail.com>
2024-08-20 10:24:39 -0500

    se subio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'pokemons', component: PokemonComponent},
+    {path: 'acercade', component: AcercadeComponent},
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../../utils/pokemon';
+import * as pokemonData from '../../../../public/json/pokemonData.json';
+import { CommonModule } from '@angular/common';
+import {randomInt} from 'crypto'
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
-  imagenes:string[]=[
-    '/greninja.png',	
-    '/charizar.png',	
-    '/gengar.png',	
-  ];
-  
+pokemons:Pokemon[]=(pokemonData as any).default;
+idPokemonSemana:number=randomInt(0, 3);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(pokemonData);
+  }
 }
