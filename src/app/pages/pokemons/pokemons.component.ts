e90f2d88d67bf309e02137bd38834323703d52f2
RichardQuing <richardquinaucho65@gmail.com>
2024-08-21 08:27:17 -0500

    SE AGRESO PRIMERA HORA

--- src/app/pages/pokemons/pokemons.component.ts
+++ src/app/pages/pokemons/pokemons.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Pokemon } from '../../utils/pokemon';
+import * as pokemonData from '../../../../public/json/pokemonData.json';
+import { routes } from '../../app.routes';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-pokemons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemons.component.html',
   styleUrl: './pokemons.component.css'
 })
 export class PokemonsComponent {
+  pokemons:Pokemon[]=(pokemonData as any).default;
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void { 
+
+  }
+  onClickPokemon(pokemon:Pokemon):void{
+    this.router.navigate(['/pokemon',pokemon.id]);
+  }
 
 }
