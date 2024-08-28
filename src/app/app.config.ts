47f007ea4b30b06c3090da3a1592b6b40d2303a5
RichardQuing <richardquinaucho65@gmail.com>
2024-08-28 10:59:27 -0500

    se subioooP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"app-pokemon-d3ceb","appId":"1:1074375494631:web:bd482e9c1276ee1ab34b42","storageBucket":"app-pokemon-d3ceb.appspot.com","apiKey":"AIzaSyAtGKLDyq3U95Gmy0D6SDeX8scocpoCXuQ","authDomain":"app-pokemon-d3ceb.firebaseapp.com","messagingSenderId":"1074375494631","measurementId":"G-7YC8S2F8K9"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { ListPostsComponent } from './pages/list-posts/list-posts.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'pokemons', component: PokemonsComponent},
     {path: 'pokemon/:id', component: PokemonComponent},
     {path: 'acercade', component: AcercadeComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'posts', component: ListPostsComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path:'**', component: Error404Component},
